6b4dbf7919087afa12920ea55d6895665099608e
Corentin <corentincasier@gmail.com>
2020-05-10 01:10:00 +0200

    remove build folder

--- TM4C123GXL/gcc/startup_gcc.d
+++
@@ -1,9 +0,0 @@
-gcc/startup_gcc.o: startup_gcc.c \
- /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
- /usr/arm-none-eabi/include/stdint.h \
- /usr/arm-none-eabi/include/machine/_default_types.h \
- /usr/arm-none-eabi/include/sys/features.h \
- /usr/arm-none-eabi/include/_newlib_version.h \
- /usr/arm-none-eabi/include/sys/_intsup.h \
- /usr/arm-none-eabi/include/sys/_stdint.h tivaware/inc/hw_nvic.h \
- tivaware/inc/hw_types.h
