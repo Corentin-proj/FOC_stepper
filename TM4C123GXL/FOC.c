7f3ddd1694fe48f025500d8305bdaeb0eeaf0dc2
Corentin <corentincasier@gmail.com>
2020-06-09 00:19:24 +0200

    add : FOC functions

---
+++ TM4C123GXL/FOC.c
@@ -0,0 +1,33 @@
+/**
+ * FOC.c
+ * Created on: June 8, 2020
+ * Author: Corentin Casier
+ * Last update: June 8, 2020
+ * This code realize the field oriented control.
+ * It means that the code compute the voltage for each phase of the stepper motor
+ * depending on the angle of the rotor, the number of poles of the rotor and
+ * the references of torque and magnetic flux.
+ *
+ */
+#include "FOC.h"
+
+// Half of a simplified inverse Park transformation (returns only the Alpha Voltage)
+float invParkAlpha(float d, float q, float theta){
+  float alpha = 0.0;
+  alpha = d*sin(theta) + q*cos(theta);
+  return alpha;
+}
+
+// Half of a simplified inverse Park transformation (returns only the Beta Voltage)
+float invParkBeta(float d, float q, float theta){
+  float beta = 0.0;
+  beta = -d*cos(theta) + q*sin(theta);
+  return beta;
+}
+
+// Convert mechanical rotor angle to electrical rotor angle.
+float mech2elec(int poles, float thetaMech){
+  float thetaElec = 0.0;
+  thetaElec = (poles/2)*thetaMech;
+  return thetaElec;
+}
---
+++ TM4C123GXL/FOC.h
@@ -0,0 +1,25 @@
+/**
+ * FOC.h
+ * Created on: June 8, 2020
+ * Author: Corentin Casier
+ * Last update: June 8, 2020
+ * This file contains the prototypes of the functions of FOC.c
+ *
+ */
+
+#ifndef FOC_H
+#define FOC_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/fpu.h"
+
+float invParkAlpha(float d, float q, float theta);
+float invParkBeta(float d, float q, float theta);
+float mech2elec(int poles, float thetaMech);
+
+#endif
